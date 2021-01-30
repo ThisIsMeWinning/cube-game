f5d03c6865f598234b32915444e70beb54f12c29
ThisIsMeWinning <daltoncclinton@gmail.com>
2021-01-30 15:38:02 -0600

    v1.0

---
+++ Cube/Assets/Scripts/PlayerName.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerName : MonoBehaviour
+{
+
+    public string playerName;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void StoreName()
+    {
+        playerName = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = playerName;
+    }
+
+}
---
+++ Cube/Assets/Scripts/Volume.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Volume : MonoBehaviour
+{
+    Text percentageText;
+
+    void Start()
+    {
+        percentageText = GetComponent<Text>();
+    }
+
+    public void textUpdate(float value)
+    {
+        percentageText.text = Mathf.RoundToInt(value * 100) + "%";
+    }
+}
